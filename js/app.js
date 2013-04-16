90fe6feacce7451d609a1c54ae4fdc7c2d19bbfa
Moboz <hannes@18techs.com>
2013-04-16 17:16:55 -0600

    made: blockOrWin and pickSide

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@ var app = angular.module('myApp', []);
 
 glb.App = function($scope, $location){
 	
-	$scope.ticTacToe = [{text: '', type:"corner"},
-					 {text: '', type:"side"},
-					 {text: '', type:"corner"},
-					 {text: '', type:"side"},
-					 {text: '', type:"center"},
-					 {text: '', type:"side"},
-					 {text: '', type:"corner"},
-					 {text: '', type:"side"},
-					 {text: '', type:"corner"}];
+	$scope.ticTacToe = [{magic:8, text: '', type:"corner"},
+					 {magic:1, text: '', type:"side"},
+					 {magic:6, text: '', type:"corner"},
+					 {magic:3, text: '', type:"side"},
+					 {magic:5, text: '', type:"center"},
+					 {magic:7, text: '', type:"side"},
+					 {magic:4, text: '', type:"corner"},
+					 {magic:9, text: '', type:"side"},
+					 {magic:2, text: '', type:"corner"}];
 					 
 	$scope.grid = function(item){
 		if (item.text == '') {
@@ -26,17 +26,44 @@ glb.App = function($scope, $location){
 	}
 
 	function compPlay(){
-		//Take the center block if available
-		var center = _.where($scope.ticTacToe, {type: "center"})[0];
-		if (center.text == '') {
-			center.text = 'O';
-		}
-		else {
-			//Take a corner spot if available
-			if (pickCorner()) {
+	
+		$scope.allX = _.where($scope.ticTacToe, {text: 'X'});
+	
+		//First check to see if can Win, then check to see if needs to block to prevent Loss 
+		if (blockOrWin() && blockOrWin($scope.allX)) {
+			//Take the center block if available
+			var center = _.where($scope.ticTacToe, {type: "center"})[0];
+			if (center.text == '') {
+				center.text = 'O';
+			}
+			else {
+				//Take a corner spot if available
+				if (pickCorner()) {
+					pickSide();
+				}
 			}
 		}
 	}
+	
+	function blockOrWin(allXO){
+		var available = _.where($scope.ticTacToe, {text: ''});
+		//if allXO is undefined = need to use O dataset, not X
+		if (!allXO) allXO = _.where($scope.ticTacToe, {text: 'O'});
+		
+		for (var i=0; i < allXO.length; i++) {
+			for (var j=i+1; j < allXO.length; j++) {
+				for (var k=0; k < available.length; k++) {
+					if (allXO[i].magic + allXO[j].magic + available[k].magic == 15) {
+						available[k].text = 'O';
+						console.log((allXO[0].text == 'O') ? 'won' : 'blocked');
+						$scope.blocked = true;
+						return false;
+					}
+				}
+			}
+		}	
+		return true;
+	}	
 	
 	function pickCorner(){
 		var corners = _.where($scope.ticTacToe, {text: '', type: "corner"});
@@ -49,5 +76,14 @@ glb.App = function($scope, $location){
 			return true;
 	}
 		
-
+	function pickSide(){
+		var sides = _.where($scope.ticTacToe, {text: '', type: "side"});
+		if (sides.length > 0) {
+			sides[_.random(sides.length-1)].text = 'O';
+			console.log('picked side');
+			return false;
+		}
+		else
+			return true;
+	}
 }
